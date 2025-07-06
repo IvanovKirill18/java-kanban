311bb8a739be9d43a53948f3457cff31415109eb
Kirill <kiril-ivanov@ya.ru>
2025-07-06 14:51:33 +0300

    "третья версия"

--- src/ru/yandex/javacourse/tasks/Epic.java
+++ src/ru/yandex/javacourse/tasks/Epic.java
@@ -1,11 +1,29 @@
 package ru.yandex.javacourse.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Epic extends Task {
+
+    private List<Integer> subtaskIds = new ArrayList<>();
+
     public Epic(String name, String description){
         super(name, description, TaskStatus.NEW);
     }
 
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId (int subtaksId) {
+        this.subtaskIds.add(subtaksId);
+    }
+
+    public void removeSubtaskId (int subtaskId) {
+        this.subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
     @Override
     public String toString() {
-    return "Epic{" + "name'" + getName() + '\'' + ", description='" + getDescription() + '\'' + ", id =" + getId() + ", status=" + getStatus() + '}';}
+    return "Epic{" + "name'" + getName() + '\'' + ", description='" + getDescription() + '\'' + ", id =" + getId() + ", status=" + getStatus() + ", subtaskIds=" + subtaskIds + '}';}
 }
